e064075ceb71d1475e6e287824ebb0c1eedd43e9
panhq <hypofiasco@gmail.com>
2022-08-15 14:39:32 +0800

    finish setup to main

--- boot/head.s
+++ boot/head.s
@@ -18,9 +18,10 @@ startup_32:
 	movl $0x10,%eax
 	mov %ax,%ds
 	mov %ax,%es
-	mov %ax,%fs
+	mov %ax,%fs			
 	mov %ax,%gs
-	lss _stack_start,%esp
+	lss _stack_start,%esp  ; > 将_stack_start存入 ss:esp 中; 即栈顶指针的指向由 0x9FF00 -> _stack_start 标号的位置 
+	; > _stack_start 标号引用的是c中的，sched.c 中的 stack_start 
 	call setup_idt
 	call setup_gdt
 	movl $0x10,%eax		; reload all the segment registers
@@ -29,6 +30,7 @@ startup_32:
 	mov %ax,%fs
 	mov %ax,%gs
 	lss _stack_start,%esp
+	; > idt 和 gdt 重新指向 head 程序（位于 0 开始的系统代码里，防止之前 在 0x90200 处会被缓冲区覆盖）
 	xorl %eax,%eax
 1:	incl %eax		; check that A20 really IS enabled
 	movl %eax,0x000000	; loop forever if it isn't
@@ -76,8 +78,8 @@ check_x87:
  *  written by the page tables.
  */
 setup_idt:
-	lea ignore_int,%edx
-	movl $0x00080000,%eax
+	lea ignore_int,%edx 
+	movl $0x00080000,%eax  ; > 默认的终端处理程序 ignore_int
 	movw %dx,%ax		/* selector = 0x0008 = cs */
 	movw $0x8E00,%dx	/* interrupt gate - dpl=0, present */
 
@@ -137,8 +139,23 @@ after_page_tables:
 	pushl $0
 	pushl $0
 	pushl $L6		# return address for main, if it decides to.
-	pushl $_main
-	jmp setup_paging
+	pushl $_main  ; > 进入 init/main.c 的 main 函数
+	; > 0 -> 0 -> 0 -> L6 -> _main
+	jmp setup_paging   ; > 最后一个指令是ret，即把栈顶的元素值当做返回地址，把 esp 寄存器（栈顶地址）所指向的内存处的值，赋值给 eip 寄存器；即跳转到 main.c 开始执行
+	; > 开启分页机制，分页是分段之后的。 分段 -> 分页 -> 物理地址； 逻辑地址 -> 分段地址(线性地址) -> 物理地址 
+	; >  线性地址：  高 10 位：   中间 10 位：   后 12 位
+	; > 		  页目录表(项)    页表(项)      偏移地址
+	; > 二级页表： 页目录表PDE、页表PTE
+	; > 分页开关：cr0的31位 0/1； 0位是保护模式的开关
+	; > MMU，中文名字叫内存管理单元，有时也叫 PMMU，分页内存管理单元。由这个部件来负责将虚拟地址转换为物理地址。软件层只需要提供 PDE、PTE
+	; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	; > 分段机制，其目的是为了为每个程序或任务提供单独的代码段（cs）、数据段（ds）、栈段（ss）。 （Intel 的保护模式下，分段机制是没有开启和关闭一说的，它必须存在）
+	; > 分页机制，其目的在于可以按需使用物理内存，同时也可以在多任务时起到隔离的作用
+
+	; > 逻辑地址：我们程序员写代码时给出的地址叫逻辑地址，其中包含段选择子和偏移地址两部分。
+	; > 线性地址：通过分段机制，将逻辑地址转换后的地址，叫做线性地址。而这个线性地址是有个范围的，这个范围就叫做线性地址空间，32 位模式下，线性地址空间就是 4G。
+	; > 物理地址：就是真正在内存中的地址，它也是有范围的，叫做物理地址空间。那这个范围的大小，就取决于你的内存有多大了。
+	; > 虚拟地址：如果没有开启分页机制，那么线性地址就和物理地址是一一对应的，可以理解为相等。如果开启了分页机制，那么线性地址将被视为虚拟地址，这个虚拟地址将会通过分页机制的转换，最终转换成物理地址。
 L6:
 	jmp L6			# main should never return here, but
 				# just in case, we know what happens.
@@ -201,9 +218,11 @@ setup_paging:
 	xorl %edi,%edi			/* pg_dir is at 0x000 */
 	cld;rep;stosl
 	movl $pg0+7,_pg_dir		/* set present bit/user r/w */
+	; >  +7 为 0x07 表示改页存在、用户可读写
 	movl $pg1+7,_pg_dir+4		/*  --------- " " --------- */
 	movl $pg2+7,_pg_dir+8		/*  --------- " " --------- */
 	movl $pg3+7,_pg_dir+12		/*  --------- " " --------- */
+	; > 覆盖了已执行的起始代码
 	movl $pg3+4092,%edi
 	movl $0xfff007,%eax		/*  16Mb - 4096 + 7 (r/w user,p) */
 	std
@@ -212,9 +231,11 @@ setup_paging:
 	jge 1b
 	xorl %eax,%eax		/* pg_dir is at 0x0000 */
 	movl %eax,%cr3		/* cr3 - page directory start */
+	; > 告诉 cr3 寄存器，0地址处就是页目录表
 	movl %cr0,%eax
 	orl $0x80000000,%eax
-	movl %eax,%cr0		/* set paging (PG) bit */
+	movl %eax,%cr0		/* set paging (PG) bit */ 
+	; > 开启分页
 	ret			/* this also flushes prefetch-queue */
 
 .align 2
